b43c111682da2f66b86aab65cb7af009dcbfb51d
Jeremy Yeaton <jdyeaton27@gmail.com>
2019-10-16 13:28:05 +0200

    Update ShuffleSyntaxStim.m

--- LSFsyntax/ShuffleSyntaxStim.m
+++ LSFsyntax/ShuffleSyntaxStim.m
@@ -1,3 +1,7 @@
+% Shuffle stimuli for LSF Syntax task
+% (c) Jeremy D. Yeaton
+% Created April 2019
+
 function ShuffleSyntaxStim(subID)
 stim = readtable('stim\stimLSFsyntax.txt','Delimiter','\t');
 
